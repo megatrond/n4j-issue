b04afe15745df9ed65f457781346957eb9ffda69
afreakk <laderud@hotmail.com>
2023-09-27 15:29:07 +0200

    init commit

---
+++ src/index.test.js
@@ -0,0 +1,161 @@
+import {readFileSync} from 'fs';
+import {ApolloServer} from '@apollo/server';
+import {join} from 'path';
+import {Neo4jGraphQL} from '@neo4j/graphql';
+import {it, describe, expect, test} from 'vitest';
+import {gql} from '@apollo/client/core';
+import neo4j from 'neo4j-driver';
+const driver = neo4j.driver(
+    process.env.NEO4J_DATABASE_HOST,
+    neo4j.auth.basic(process.env.NEO4J_DATABASE_USERNAME, process.env.NEO4J_DATABASE_PASSWORD)
+);
+const laterResolvedValues = (type) => {
+    const values = {};
+    const get = (key) => {
+        expect(values[key]).toBeTypeOf(type);
+        return values[key];
+    };
+    const set = (key, value) => {
+        values[key] = value;
+    };
+    return {set, get};
+};
+
+describe('testing graphql', async () => {
+    await driver.session().run(` match (n) detach delete n`);
+    const myKratosId = Math.random().toString(36).slice(2, 7);
+    const neo4jGraphql = new Neo4jGraphQL({typeDefs: readFileSync(join(__dirname, 'schema.graphql'), 'utf8').toString(), driver});
+    const neoSchema = await neo4jGraphql.getSchema();
+    const apolloServer = new ApolloServer({
+        schema: neoSchema,
+        introspection: true,
+    });
+    const l = laterResolvedValues('string');
+    it('create tenant', async () => {
+        const ADD_TENANT = gql`
+            mutation addTenant($input: [TenantCreateInput!]!) {
+                createTenants(input: $input) {
+                    tenants {
+                        id
+                        admins {
+                            userId
+                        }
+                    }
+                }
+            }
+        `;
+        const tenantVariables = {
+            input: {
+                admins: {
+                    create: {
+                        node: {userId: myKratosId},
+                    },
+                }
+            },
+        };
+        const r = await apolloServer.executeOperation(
+            {query: ADD_TENANT, variables: tenantVariables},
+            //  we actually currently NEED to have adminId match the created admin, cause we query admins
+            //  but  probably overlords  should be able to query all users?
+            {contextValue: {jwt: {roles: ['overlord'], id: myKratosId, sub: ''}}}
+        );
+        expect(r).toMatchObject({
+            body: {
+                singleResult: {
+                    data: {
+                        createTenants: {
+                            tenants: [
+                                {id: expect.any(String), admins: [{userId: myKratosId}]},
+                            ],
+                        },
+                    },
+                },
+            },
+        });
+        l.set('tenantId', r.body.singleResult.data.createTenants.tenants[0].id);
+        l.set('userId', r.body.singleResult.data.createTenants.tenants[0].admins[0].userId);
+    });
+    it('create garage', async () => {
+        const ADD_GARAGES = gql`
+            mutation addGarages($input: [GarageCreateInput!]!) {
+                createGarages(input: $input) {
+                    garages {
+                        id
+                    }
+                }
+            }
+        `;
+        const garageInput = (tenantId) => ({
+            tenant: {
+                connect: {
+                    where: {
+                        node: {
+                            id: tenantId,
+                        },
+                    },
+                },
+            },
+        });
+        const r = await apolloServer.executeOperation(
+            {query: ADD_GARAGES, variables: {input: garageInput(l.get('tenantId'))}},
+            {contextValue: {jwt: {roles: [], id: l.get('userId'), sub: ''}}}
+        );
+        expect(r).toMatchObject({
+            body: {
+                singleResult: {data: {createGarages: {garages: [{id: expect.any(String)}]}}},
+            }
+        });
+        l.set('garageId', r.body.singleResult.data.createGarages.garages[0].id);
+    });
+    it('create vehiclecard', async () => {
+        const ADD_VEHICLE_CARD = gql`
+            mutation addVehicleCard($input: [VehicleCardCreateInput!]!) {
+                createVehicleCards(input: $input) {
+                    vehicleCards {
+                        id
+                    }
+                }
+            }
+        `;
+        const vehiclecardInput = ({garageId, tenantId}) => ({
+            tenant: {
+                connect: {
+                    where: {
+                        node: {
+                            id: tenantId,
+                        },
+                    },
+                },
+            },
+            // if i comment out this, it works
+            garages: {
+                connect: {
+                    where: {
+                        node: {
+                            id: garageId,
+                        },
+                    },
+                },
+            },
+        });
+        const r = await apolloServer.executeOperation(
+            {
+                query: ADD_VEHICLE_CARD,
+                variables: {
+                    input: vehiclecardInput({
+                        garageId: l.get('garageId'),
+                        tenantId: l.get('tenantId'),
+                    }),
+                },
+            },
+            {contextValue: {jwt: {roles: [], id: l.get('userId'), sub: ''}}}
+        );
+        expect(r).toMatchObject({
+            body: {
+                singleResult: {
+                    data: {createVehicleCards: {vehicleCards: [{id: expect.any(String)}]}},
+                },
+            },
+        });
+    });
+})
